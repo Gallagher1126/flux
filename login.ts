157303ff44ff1fb439e1f52fa0634d84e64e9504
葛高召 <30767105+Gallagher1126@users.noreply.github.com>
2018-11-14 15:03:05 +0800

    Create login.ts
      用 userAction 发送登录请求

---
+++ login.ts
@@ -0,0 +1,29 @@
+'use strict';
+import {UserAction} from "../../framework/action/UserAction";
+import {ActionType} from "../../framework/constant/ActionType";
+import {NativeStorage} from "ionic-native";
+
+export class Login  {
+
+  username = "";
+  password = "";
+
+  constructor() {
+  }
+
+  private _doLogin(username: string, password: string) {
+    return new Promise((resolve, reject) => {
+      let action: UserAction = new UserAction(ActionType.ACTION_LOGIN, "xxxx");
+      action.username = username;
+      action.password = password;
+      action.callback = (error, responseJson) => {
+        if (!error && undefined != responseJson.ret && responseJson.ret == 0) {
+          // 登陆成功 
+        }
+      };
+      action.dispatch();
+    });
+  }
+
+
+}
